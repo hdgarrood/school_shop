b78ed55837af889240b1d538e882a327ca40b1a0
Harry Garrood <hdgarrood@gmail.com>
2012-09-27 22:34:04 +0100

    use change instead of up/down

--- db/migrate/20120927200823_create_garment_descriptions.rb
+++ db/migrate/20120927200823_create_garment_descriptions.rb
@@ -1,11 +1,7 @@
 class CreateGarmentDescriptions < ActiveRecord::Migration
-  def up
+  def change
     create_table :garment_descriptions do |t|
       t.string :description
     end
-  end
-
-  def down
-    drop_table :garment_descriptions
   end
 end
