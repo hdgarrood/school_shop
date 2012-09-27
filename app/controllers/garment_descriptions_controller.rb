b9a8426d6c9ce143d0b1a85331270e0bf050e98d
Harry Garrood <hdgarrood@gmail.com>
2012-09-27 22:00:49 +0100

    adding more stuff

--- app/controllers/garment_descriptions_controller.rb
+++ app/controllers/garment_descriptions_controller.rb
@@ -6,4 +6,23 @@ class GarmentDescriptionsController < ApplicationController
   def new
     @garment_description = GarmentDescription.new
   end
+
+  def create
+    @garment_description = GarmentDescription.new(params[:garment_description])
+
+    if @garment_description.save
+      redirect_to(@garment_description,
+                  :notice => 'Garment description was successfully created.')
+    else
+      render :action => 'new'
+    end
+  end
+
+  def show
+    @garment_description = GarmentDescription.find(params[:id])
+  end
+
+  def edit
+    @garment_description = GarmentDescription.find(params[:id])
+  end
 end
