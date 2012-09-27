ac5b4b274d91b11fa5bc06e1348bdd09261b74a8
Harry Garrood <hdgarrood@gmail.com>
2012-09-27 22:29:39 +0100

    adding destroy method

--- app/controllers/garment_descriptions_controller.rb
+++ app/controllers/garment_descriptions_controller.rb
@@ -25,4 +25,21 @@ class GarmentDescriptionsController < ApplicationController
   def edit
     @garment_description = GarmentDescription.find(params[:id])
   end
+
+  def update
+    @garment_description = GarmentDescription.find(params[:id])
+
+    if @garment_description.update_attributes(params[:garment_description])
+      redirect_to(@garment_description,
+                 :notice => 'Garment description was successfully updated.')
+    else
+      render :action => 'edit'
+    end 
+  end
+
+  def destroy
+    @garment_description = GarmentDescription.find(params[:id])
+    @garment_description.destroy
+    redirect_to garment_descriptions_url
+  end
 end
