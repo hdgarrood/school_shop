8ac756d86c2863282ee118091e1deecb836fd91d
Harry Garrood <hdgarrood@gmail.com>
2012-09-27 21:45:47 +0100

    on second thoughs -- screw i18n

--- app/controllers/garment_descriptions_controller.rb
+++ app/controllers/garment_descriptions_controller.rb
@@ -2,4 +2,8 @@ class GarmentDescriptionsController < ApplicationController
   def index
     @garment_descriptions = GarmentDescription.all
   end
+
+  def new
+    @garment_description = GarmentDescription.new
+  end
 end
