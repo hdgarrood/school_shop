c3ddfc6d54e2cb4fc9dbf207be06b25c90b55baa
Harry Garrood <hdgarrood@gmail.com>
2012-09-27 21:03:29 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolShop::Application.config.secret_token = 'a660e9ae206d233d73dbae22abd06dd87372f496186af9242564ad2efe36bb7013e96297f72b9f7fa39a4aebcefa4312b569391be90c9fb7e9a62bd090bdcca0'
