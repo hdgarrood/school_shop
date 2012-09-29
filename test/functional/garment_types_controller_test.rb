b3aedd07962cf5a606c24298d403b2b493840924
Harry Garrood <hdgarrood@gmail.com>
2012-09-29 22:21:18 +0100

    fix (hopefully) last remnant of GarmentDescription

--- test/functional/garment_types_controller_test.rb
+++ test/functional/garment_types_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class GarmentDescriptionsControllerTest < ActionController::TestCase
+class GarmentTypesControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
