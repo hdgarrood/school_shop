26a77ce5e3788fa000d833d0538b8efae1693bed
Harry Garrood <hdgarrood@gmail.com>
2012-09-29 22:22:47 +0100

    add test for size converter

--- test/unit/size_converter_test.rb
+++ test/unit/size_converter_test.rb
@@ -10,4 +10,9 @@ class SizeConverterTest < ActiveSupport::TestCase
     result = @sc.years_to_height_cm(6)
     assert_equal(result, 116)
   end
+
+  def test_returns_nil_when_out_of_bounds
+    result = @sc.years_to_height_cm(5000)
+    assert_nil(result)
+  end
 end
