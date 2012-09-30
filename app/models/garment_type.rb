0f7dbbac1588b27532bb8a6bb2e0fd29c933cc20
Harry Garrood <hdgarrood@gmail.com>
2012-09-30 14:31:09 +0100

    remove redundant presence => true

--- app/models/garment_type.rb
+++ app/models/garment_type.rb
@@ -1,7 +1,6 @@
 class GarmentType < ActiveRecord::Base
   attr_accessible :name, :measured_by
   validates_presence_of :name
-  validates :measured_by, :presence => true,
-                          :inclusion => { :in => %w(height waist chest collar) }
+  validates :measured_by, :inclusion => { :in => %w(height waist chest collar) }
   has_many :garments
 end
