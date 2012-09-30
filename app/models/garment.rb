2db9b9734bd7d50d88d1e38efe0c92fadbbde2a3
Harry Garrood <hdgarrood@gmail.com>
2012-09-30 16:34:57 +0100

    add size accessor to Garment

--- app/models/garment.rb
+++ app/models/garment.rb
@@ -10,4 +10,15 @@ class Garment < ActiveRecord::Base
   def sold?
     !sold_at.nil?
   end
+
+  # getter and setter for a size -- since a SizeRange can behave like a
+  # Size if the upper bound is the same as the lower bound, we can
+  # make the model behave as though it has a size attribute
+  def size
+    size_range.to_size
+  end
+
+  def size=(size)
+    size_range = SizeRange.from_size(size)
+  end
 end
