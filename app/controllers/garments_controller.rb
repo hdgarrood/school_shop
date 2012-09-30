5e01e97979c8781dd770a247cda6b3ef1e598eda
Harry Garrood <hdgarrood@gmail.com>
2012-09-30 20:05:19 +0100

    add create for garments

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -6,4 +6,14 @@ class GarmentsController < ApplicationController
     @garment = Garment.new
     @garment_types = GarmentType.all
   end
+
+  def create
+    @garment = Garment.new(params[:garment])
+    if @garment.save
+      flash[:notice] = "Garment added."
+      redirect_to @garment
+    else
+      render :action => 'new'
+    end
+  end
 end
