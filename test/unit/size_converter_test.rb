acf168dbe370c712dc3e395b80b173c9c91485d5
Harry Garrood <hdgarrood@gmail.com>
2012-09-30 01:32:11 +0100

    add Size class and make SizeConverter a module

--- test/unit/size_converter_test.rb
+++ test/unit/size_converter_test.rb
@@ -1,28 +1,24 @@
 require 'test_helper'
-require 'size_converter'
+require 'size'
 
 class SizeConverterTest < ActiveSupport::TestCase
-  def setup
-    @sc = SizeConverter.new
-  end
-
   def test_converting_years_to_height_cm
-    result = @sc.years_to_height_cm(6)
+    result = Size.years_to_height_cm(6)
     assert_equal(116, result)
   end
 
   def test_converting_from_inches_to_years
-    result = @sc.height_inches_to_years(38.5)
+    result = Size.height_inches_to_years(38.5)
     assert_equal(3, result)
   end
 
   def test_returns_nil_when_out_of_bounds
-    result = @sc.years_to_height_cm(5000)
+    result = Size.years_to_height_cm(5000)
     assert_nil(result)
   end
 
   def test_interpolates
-    result = @sc.chest_cm_to_years(84)
+    result = Size.chest_cm_to_years(84)
     assert_equal(13.5, result)
   end
 end
