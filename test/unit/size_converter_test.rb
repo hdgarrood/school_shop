21ba7789bd5745b9c86a584ad606139eef9ec57f
Harry Garrood <hdgarrood@gmail.com>
2012-09-30 01:40:56 +0100

    refactoring Size and SizeConverter

--- test/unit/size_converter_test.rb
+++ test/unit/size_converter_test.rb
@@ -3,22 +3,22 @@ require 'size'
 
 class SizeConverterTest < ActiveSupport::TestCase
   def test_converting_years_to_height_cm
-    result = Size.years_to_height_cm(6)
+    result = Size.convert(6, 'years', 'height_cm')
     assert_equal(116, result)
   end
 
   def test_converting_from_inches_to_years
-    result = Size.height_inches_to_years(38.5)
+    result = Size.convert(38.5, 'height_inches', 'years')
     assert_equal(3, result)
   end
 
   def test_returns_nil_when_out_of_bounds
-    result = Size.years_to_height_cm(5000)
+    result = Size.convert(5000, 'years', 'height_cm')
     assert_nil(result)
   end
 
   def test_interpolates
-    result = Size.chest_cm_to_years(84)
+    result = Size.convert(84, 'chest_cm', 'years')
     assert_equal(13.5, result)
   end
 end
