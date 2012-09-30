645ab48ded40cbddc8e6abf787ad1f34659b124d
Harry Garrood <hdgarrood@gmail.com>
2012-09-30 16:42:29 +0100

    add comparison tests for Size

--- test/unit/size_test.rb
+++ test/unit/size_test.rb
@@ -22,8 +22,25 @@ class SizeTest < ActiveSupport::TestCase
     assert(s1 != s3)
   end
 
-  def test_comparison
-    # TODO
+  def test_comparison_with_same_units
+    s1 = Size.new(12.5, 'collar_inches')
+    s2 = Size.new(12.25, 'collar_inches')
+    s3 = Size.new(12.75, 'collar_inches')
+
+    assert_equal(0, s1 <=> s1)
+    assert_equal(1, s1 <=> s2)
+    assert_equal(-1, s1 <=> s3)
+  end
+
+  def test_comparison_with_different_units
+    s1 = Size.new(24, 'chest_inches')
+    s2 = Size.new(6, 'years')
+    s3 = Size.new(58, 'chest_cm')
+    s4 = Size.new(8, 'years')
+
+    assert_equal(0, s1 <=> s2)
+    assert_equal(1, s1 <=> s3)
+    assert_equal(-1, s1 <=> s4)
   end
 
   def test_to_s_when_attributes_are_nil
