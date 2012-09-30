f6e2392019437fc22e4f3ffa4e967d9e028720ab
Harry Garrood <hdgarrood@gmail.com>
2012-09-30 02:12:17 +0100

    reorganise

--- lib/size.rb
+++ lib/size.rb
@@ -1,27 +1,3 @@
-require 'size_converter'
-
-class Size
-  extend SizeConverter
-  include Comparable
-  attr_reader :value, :unit
-
-  def initialize(value, unit)
-    @value, @unit = value, unit
-  end
-
-  def convert_to(other_unit)
-    Size.convert(@value, @unit, other_unit)
-  end
-
-  def ==(other_size)
-    @value == other_size.value && @unit == other_size.unit
-  end
-
-  def <=>(other_size)
-    if @unit == other_size.unit
-      @value <=> other_size.value
-    else
-      @value <=> other_size.convert_to(@unit)
-    end
-  end
-end
+require 'size/size_range'
+require 'size/size_converter'
+require 'size/size'
