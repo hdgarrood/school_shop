1616407edff9447141b9691c3085e8709e3763c4
Harry Garrood <hdgarrood@gmail.com>
2012-10-03 00:43:14 +0100

    add method to render a size

--- app/helpers/garments_helper.rb
+++ app/helpers/garments_helper.rb
@@ -1,2 +1,15 @@
 module GarmentsHelper
+  def render_size(size)
+    other_units = Size.unit_values
+    other_units.delete(size.unit)
+
+    other_sizes = other_units.map { |u| size.convert_to(u) }
+
+    title = "This is the same as:\n"
+    title << other_sizes.join("\n")
+
+    content_tag(:span, :class => 'size', :title => title) do
+      size.to_s
+    end
+  end
 end
