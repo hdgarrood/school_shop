6def8ce8bcc4244367822557436ce5d5489c8cb5
Harry Garrood <hdgarrood@gmail.com>
2012-10-03 23:49:37 +0100

    start refactoring sizes (again)

--- app/models/garment.rb
+++ app/models/garment.rb
@@ -1,29 +1,18 @@
 class Garment < ActiveRecord::Base
   attr_accessible :sold_at,
                   :garment_type_id,
-                  :size
+                  :size_lower_bound,
+                  :size_upper_bound,
+                  :size_unit
 
   belongs_to :garment_type
 
-  composed_of :size,
-              :mapping => [%w(size_lbound lbound),
-                           %w(size_ubound ubound),
-                           %w(size_unit unit)],
-              :converter => Proc.new { |params| Size.new(params) }
-
   validates_presence_of :garment_type
-  validates_presence_of :size
-  validates :size_lbound, :numericality => { :less_than_or_equal_to => :size_ubound }
-  validates :size_ubound, :numericality => :true
+  validates :size_lower_bound, :numericality => { :less_than_or_equal_to => :size_upper_bound }
+  validates :size_upper_bound, :numericality => :true
   validates :size_unit, :inclusion => { :in => Size.unit_values }
 
   def sold?
     !sold_at.nil?
-  end
-
-  def convertable_size
-    params = size.to_hash
-    params[:measurement] = garment_type.measured_by
-    ConvertableSize.new(params)
   end
 end
