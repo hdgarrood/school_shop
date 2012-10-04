212ca3c38d0f94680711da15e88a82aec190bf61
Harry Garrood <hdgarrood@gmail.com>
2012-10-04 19:03:38 +0100

    changing some stuff

--- app/models/garment.rb
+++ app/models/garment.rb
@@ -18,19 +18,14 @@ class Garment < ActiveRecord::Base
     Size.new(size_lower_bound,
              size_upper_bound,
              'normalized',
-             garment_type.measured_by).convert_to(size_unit)
+             garment_type.measured_by)
   end
 
   def size=(new_size)
-    # ensure that this works when +new_size+ is either a Size or a hash
-    normalized_size = Size.new(new_size.to_hash).convert_to('normalized')
-
-    # the following should ensure that the size saved to the DB is
-    # always normalized.
-    if normalized_size
-      self.size_lower_bound = normalized_size.lower_bound
-      self.size_upper_bound = normalized_size.upper_bound
-      self.size_unit = normalized_size.unit
+    if (size = Size.new(new_size.to_hash))
+      self.size_lower_bound = size.lower_bound
+      self.size_upper_bound = size.upper_bound
+      self.size_unit = size.unit
     end
   end
 end
