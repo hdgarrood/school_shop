e688f711c9f408182f94af74a656c2feae747d22
Harry Garrood <hdgarrood@gmail.com>
2012-10-07 22:59:20 +0100

    adding search things

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SchoolShop::Application.routes.draw do
+  match "search" => 'search#index', :via => :get
   resources :garment_types
   resources :garments do
     member { match :sell, :via => [:get, :put] }
