75b7de3539bbd3374164bcbe0df1a05d3d626f73
Harry Garrood <hdgarrood@gmail.com>
2012-10-07 05:13:26 +0100

    add price_field to forms

--- app/models/garment.rb
+++ app/models/garment.rb
@@ -4,6 +4,8 @@ class Garment < ActiveRecord::Base
 
   belongs_to :garment_type
   validates_presence_of :garment_type
+  validates :price, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ },
+                    :numericality => { :greater_than => 0 }
 
   def sold?
     !sold_at.nil?
