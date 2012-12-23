29e42317e7183f3f43b493920a5a69b6b1faff29
Harry Garrood <hdgarrood@gmail.com>
2012-12-23 18:53:19 +0000

    allow links in the output for sales

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,25 +1,41 @@
 require 'csv'
 
 module SalesHelper 
-  def csv_attrs(garment)
-    {
-      :id => garment.id,
-      :type => garment.garment_type,
-      :price => number_to_currency(garment.price),
-      :received_on => l(garment.created_at.to_date, :format => :long),
-      :sold_on => l(garment.sold_at.to_date, :format => :long)
-    }
+  def render_garment_id(garment, format)
+    case format
+    when :html
+      link_to(garment.to_short_s, garment)
+    when :csv
+      garment.id
+    end
+  end
+
+  # list of arrays, where the first element of each is the header, and the
+  # second is the value
+  def garment_attributes(garment, format = :html)
+    [
+      [t('garments.name'), 
+        render_garment_id(garment, format)],
+      [t('garment_types.short_name'),
+        garment.garment_type],
+      [t('price'),
+        number_to_currency(garment.price)],
+      [t('garments.created_at'),
+        l(garment.created_at.to_date, :format => :long)],
+      [t('garments.sold_at'),
+        l(garment.sold_at.to_date, :format => :long)]
+    ]
   end
 
   def sales_to_csv(sold_garments)
-    str = csv_attrs(sold_garments.first).
-      keys.
+    str = garment_attributes(sold_garments.first, :csv).
+      map(&:first).
       map(&:to_s).
       map(&:humanize).
       to_csv
     str << CSV.generate do |csv|
       sold_garments.each do |sg|
-        csv << csv_attrs(sg).values
+        csv << garment_attributes(sg, :csv).map(&:last)
       end
     end
     str.html_safe
